e2a25f8e48a1c543838cc176ffb1261d9816338e
firstChairCoder <md1qq7@gmail.com>
2022-10-22 06:42:00 +0100

    refactor: enable absolute path resolution

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,34 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx'
+          ],
+          root: ['./'],
+          alias: {
+            '~/assets': './assets',
+            '~/components': './src/components',
+            '~/constants': './src/constants',
+            '~/data': './src/data',
+            '~/env': './env',
+            '~/screens': './src/screens',
+            '~/navigation': './src/navigation',
+            '~/redux': './src/redux',
+            '~/translations': './src/translations',
+            "underscore": 'lodash'
+          },
+        },
+      ], 'react-native-reanimated/plugin']
   };
 };
