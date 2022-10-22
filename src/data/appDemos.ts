94dbd36cb5d39d0ec60cbcc9d9f78592a668545f
firstChairCoder <md1qq7@gmail.com>
2022-10-22 06:41:22 +0100

    chore: add placeholder images for HomeScreen demo

---
+++ src/data/appDemos.ts
@@ -0,0 +1,18 @@
+export const DEMOS = [
+  {
+    name: "Clock",
+    component: "ClockScreen"
+  },
+  {
+    name: "Weather",
+    component: ""
+  },
+  {
+    name: "Blank",
+    component: ""
+  },
+  {
+    name: "Blank2",
+    component: ""
+  }
+];
